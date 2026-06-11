9494e67dc05c9dab4c725c8c88ab7a8f7e3f7708
LatinStreamNic <Latinstreamnic@gmail.com>
2026-06-11 16:32:08 +0000

    Update coupon discounts and codes for devices

--- js/data/coupons.js
+++ js/data/coupons.js
@@ -34,38 +34,38 @@ const coupons={
 };
 
 const couponDeviceDiscounts={
-  'Munsial2026':{
+  'MUNDIAL2026':{
     defaultByDevice:{
-      '1 Dispositivo':10,
-      '2 Dispositivos':15,
-      '3 Dispositivos':20,
-      '5 Dispositivos':25,
-      '6 Dispositivos':20
+      '1 Dispositivo':0,
+      '2 Dispositivos':3,
+      '3 Dispositivos':5,
+      '5 Dispositivos':7,
+      '6 Dispositivos':5
     },
     products:{
       'Stella TV':{
-      '1 Dispositivo':10,
-      '2 Dispositivos':15,
-      '3 Dispositivos':20},
+      '1 Dispositivo':0,
+      '2 Dispositivos':3,
+      '3 Dispositivos':5},
 
       'WeibTV (GVS)':{
-      '1 Dispositivo':10, 
-      '3 Dispositivos':20,
-      '5 Dispositivos':25},
+      '1 Dispositivo':0, 
+      '3 Dispositivos':3,
+      '5 Dispositivos':7},
 
       'Tele-Latino':{
-      '1 Dispositivo':10,
-      '6 Dispositivos':20},
+      '1 Dispositivo':0,
+      '6 Dispositivos':5},
 
       'VeltixTV':{
-      '1 Dispositivo':10,
-      '2 Dispositivos':15,
-      '3 Dispositivos':20},
+      '1 Dispositivo':4,
+      '2 Dispositivos':5,
+      '3 Dispositivos':7},
 
       '+Streaming':{
-        '1 Dispositivo':10,
-        '2 Dispositivos':15,
-        '3 Dispositivos':20}
+        '1 Dispositivo':0,
+        '2 Dispositivos':0,
+        '3 Dispositivos':0}
     }
   },
   'LATINSTREAM':{
